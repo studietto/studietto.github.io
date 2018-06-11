40556d86a4abcbb6102773868b1f9d8f0249bf97
mbellinaso <mbellinaso@gmail.com>
2018-06-11 18:49:11 +0200

    Add files via upload

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1528734103.65
+importScripts('https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://azcommercedemofb19f.blob.core.windows.net/miscfiles/icon.png',
+  defaultTitle: 'AzCommerceDemo',
+  defaultActionURL: 'http://www.azcommercedemo.com',
+  appKey: 'hE355jqYSLuSjo9ArwvrPQ',
+  token: 'MTpoRTM1NWpxWVNMdVNqbzlBcnd2clBROm1mYmFwR0tOZU1rZ2tTOUQxSU51Nng4UDdHN1NNcmN3Q0FHRUtYODd3ZWc',
+  vapidPublicKey: 'BO1X3qTEbgkJ9VdDkkmuDuLa5lhkqG2bOPrl_FqcmHfeOMeFyJrgW8R8IOOR2Nrjw4_n6XmcTn3u6RGargdlopQ='
+})
