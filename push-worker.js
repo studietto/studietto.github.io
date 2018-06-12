b2cbf88fe89ee9739cfff36c18e0b351190d4541
mbellinaso <mbellinaso@gmail.com>
2018-06-12 20:37:39 +0200

    Update push-worker.js

--- push-worker.js
+++ push-worker.js
@@ -1,7 +1,7 @@
 // 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1528734103.65
 importScripts('https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
-  defaultIcon: 'https://azcommercedemofb19f.blob.core.windows.net/miscfiles/icon.png',
+  defaultIcon: 'https://azcommercedemofb19f.blob.core.windows.net/miscfiles/store.png',
   defaultTitle: 'AzCommerceDemo',
   defaultActionURL: 'http://www.azcommercedemo.com',
   appKey: 'hE355jqYSLuSjo9ArwvrPQ',
